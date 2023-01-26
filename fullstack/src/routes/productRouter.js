c3d7bc0b84e9ce8d97184906b4da0e35d556ff47
aakashdeveloper <ahanda205@gmail.com>
2023-01-26 09:07:24 +0530

    apicalling

--- fullstack/src/routes/productRouter.js
+++ fullstack/src/routes/productRouter.js
@@ -24,6 +24,28 @@ function router(menu){
         
     })
 
+
+    productRouter.route('/category/:id')
+    .get(function(req,res){
+        //let id = req.params.id;
+        let {id} = req.params
+        mongodb.connect(url, function(err,dc){
+            if(err) {
+                res.status(500).send('Error While connecting')
+            }else{
+                let dbObj = dc.db('janmrng');
+                dbObj.collection('products').find({category_id:Number(id)}).toArray(function(err,products){
+                    if(err){
+                        res.status(203).send('Error While Fetching')
+                    }else{
+                        res.render('products',{products,title:'Products Page',menu})
+                    }
+                })
+            }
+        })
+    })
+
+
     productRouter.route('/details')
         .get(function(req,res){
         res.send("Products Details")
---
+++ weatherapp/app.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let request = require('request');
+let app = express();
+let port = process.env.PORT || 7660;
+
+app.use(express.static(__dirname+'/public'));
+app.set('views','./src/views')
+app.set('view engine','ejs');
+
+app.get('/weather',(req,res) => {
+    let city = req.query.city
+    let url = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+    // calling api
+    request(url,(err,response)=>{
+        if(err) throw err;
+        //res.send(response.body)
+        const output = JSON.parse(response.body)
+        res.render('index',{title:'Weather App',result:output})
+    })
+})
+
+
+app.listen(port,(err) => {
+    if(err) throw err;
+    console.log(`Running on port ${port}`)
+})
