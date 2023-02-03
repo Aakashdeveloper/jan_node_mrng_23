88345351ee7d9c2358ce71ba5fc9bd143a58d99f
aakashdeveloper <ahanda205@gmail.com>
2023-02-03 09:00:04 +0530

    nodees6

---
+++ Zomatoapi/apidoc.md
@@ -0,0 +1,13 @@
+// Page1 (search Page)
+> List of city
+> List of restaurants
+> Restaurants wrt City
+> List of MealTypes
+
+// Page2 (listing Page)
+
+// Page3 (details Page)
+
+// Page4 (placeOrder Page)
+
+// Page5 (OrderListing Page)
--- rediswithmongo/app.js
+++ rediswithmongo/app.js
@@ -1,12 +1,46 @@
 let express = require('express');
 let redis = require('redis');
-let mongodb = require('mongodb').MongoClient;
+let mongo = require('mongodb').MongoClient;
 let mongourl = "mongodb://localhost:27017"
 const port = process.env.PORT || 7521;
 const app = express();
 const client = redis.createClient({
     host: 'localhost',
     port:6379
+})
+
+app.get('/health',(req,res) => {
+    res.send('ok')
+})
+
+//
+app.get('/data',(req,res)=>{
+    const userInput = req.query.color.trim()
+    return client.get(userInput,(err,result) => {
+        //if data in redis
+        if(result){
+            console.log(">>>>>>1")
+            const output = JSON.parse(result);
+            res.send(output)
+        }else{
+            // as data is not in redis fetch from mongodb
+            mongo.connect(mongourl,(err,dc) => {
+                console.log(">>>>>>2")
+                if(err){
+                    res.send('Error While Connecting')
+                } else {
+                    console.log(">>>>>>3")
+                    let dbObj = dc.db('janmrng')
+                    dbObj.collection('products').find({'Color':userInput}).toArray((err,result) => {
+                        if(err) throw err
+                        //firt save response in redis
+                        client.setex(userInput,3600,JSON.stringify({source:'Redis Cache',result}))
+                        res.send({source:'Mongodb',result})
+                    })
+                }
+            })
+        }
+    })
 })
 
 
