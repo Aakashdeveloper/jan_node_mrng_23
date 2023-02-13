2aa5d0d6ccf74bcf1dfd81642574ff15ccdee05d
aakashdeveloper <ahanda205@gmail.com>
2023-02-13 08:57:53 +0530

    page3 api

--- Zomatoapi/apidoc.md
+++ Zomatoapi/apidoc.md
@@ -20,13 +20,19 @@
 * http://localhost:1009/filter/2?cuisineId=4
 
 > (Get) Restaurants wrt to meal + cost
+* http://localhost:1009/filter/1?lcost=500&hcost=1000
 
 > (Get) Restaurants sort wrt to cost
+* http://localhost:1009/filter/1?cuisineId=2&sort=-1
+
 > (Get) Pagination
+* http://localhost:1009/filter/1?cuisineId=2&skip=2&limit=2
 
 // Page3 (details Page)
 > (Get) Restaurants Details
+* http://localhost:1009/details/6288d22dbb17b75750d11ca5
 > (Get) Menu Wrt to restaurants
+* http://localhost:1009/menu/3
 
 // Page4 (placeOrder Page)
 > (POST) Menu details
--- Zomatoapi/app.js
+++ Zomatoapi/app.js
@@ -61,17 +61,42 @@ app.get('/restaurants',(req,res) => {
 
 //filters
 app.get('/filter/:mealId',(req,res) => {
-    let query = {}
+    let sort = {cost:1}
+    let query = {};
+    let skip = 0;
+    let limit = 1000000
     let mealId = Number(req.params.mealId);
     let cuisineId = Number(req.query.cuisineId);
+    let hcost = Number(req.query.hcost);
+    let lcost = Number(req.query.lcost);
 
-    if(cuisineId){
+    if(req.query.skip && req.query.limit){
+        skip = Number(req.query.skip);
+        limit = Number(req.query.limit);
+    }
+
+    if(req.query.sort){
+        sort={cost:req.query.sort}
+    }
+
+    if(cuisineId & hcost && lcost){
+        query = {
+            'mealTypes.mealtype_id':mealId,
+            'cuisines.cuisine_id':cuisineId,
+            $and:[{cost:{$gt:lcost,$lt:hcost}}]
+        }
+    }else if(cuisineId){
         query = {
             'mealTypes.mealtype_id':mealId,
             'cuisines.cuisine_id':cuisineId
         }
+    }else if(hcost && lcost){
+        query = {
+            'mealTypes.mealtype_id':mealId,
+            $and:[{cost:{$gt:lcost,$lt:hcost}}]
+        }
     }
-    db.collection('restaurants').find(query).toArray((err,data) => {
+    db.collection('restaurants').find(query).sort(sort).skip(skip).limit(limit).toArray((err,data) => {
         res.status(200).send(data)
     })
 })
@@ -82,6 +107,26 @@ app.get('/mealTypes',(req,res) => {
         res.status(200).send(data)
     })
 })
+
+
+// details of restaurants
+app.get('/details/:id',(req,res) => {
+    let _id = mongo.ObjectId(req.params.id)
+    db.collection('restaurants').find({_id:_id}).toArray((err,data) => {
+        if(err) throw err;
+        res.status(200).send(data)
+    })
+})
+
+//menu of restaurants
+app.get('/menu/:id',(req,res) => {
+    let restId = Number(req.params.id);
+    db.collection('menu').find({restaurant_id:restId}).toArray((err,data) => {
+        if(err) throw err;
+        res.status(200).send(data)
+    })
+})
+
 
 MongoClient.connect(mongoUrl,(err,client) => {
     if(err) console.log(`Error While Connecting to mongo`);
