c8fc377161f4bae76754aae707b7638a3eaadfd7
aakashdeveloper <ahanda205@gmail.com>
2023-03-02 08:51:26 +0530

    loginwithgit

--- loginwithgit/app.js
+++ loginwithgit/app.js
@@ -7,6 +7,44 @@ let port = process.env.PORT ||1100;
 
 app.use(cors())
 
+app.get('/',(req,res) =>{
+    res.send('<a href="https://github.com/login/oauth/authorize?client_id=e2040c714d78f4087ae3"> Login With Github</a>')
+});
+
+
+app.get('/profile',(req,res) => {
+    const code = req.query.code;
+    if(!code){
+        res.send({
+            success:false,
+            message:'Error While Login'
+        })
+    }
+    superagent
+        .post('https://github.com/login/oauth/access_token')
+        .send({
+            client_id:'',
+            client_secret:'',
+            code:code
+        })
+        .set('Accept','application/json')
+        .end((err,result) => {
+            if(err) throw err;
+            let access_token = result.body.access_token;
+            const option = {
+                uri:'https://api.github.com/user',
+                method:'GET',
+                headers:{
+                    'Accept':'application/json',
+                    'Authorization':`Bearer ${access_token}`,
+                    'User-Agent':'mycode'
+                }
+            }
+            request(option,(err,response,body) => {
+                res.send(body)
+            })
+        })
+})
 
 
 
